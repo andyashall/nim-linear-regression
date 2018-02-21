b7c58cd0226d5210e9784c94c2433df1fbf58067
andy <andy@ashall.co>
2018-02-21 13:53:46 +0000

    Completed BGD for iris

--- iris.nim
+++ iris.nim
@@ -1,8 +1,9 @@
 import neo
 import strutils
 from random import random, randomize
-import times, os, parsecsv, streams
+import times, os, parsecsv, streams, math
 
+# Reads csv file to matrix ("filepath", n_rows, n_cols) : how can it detect n_rows and n_cols?
 proc read_csv(fn: string, rows: int, cols: int): (Matrix[float64], Matrix[float64]) =
   var x = zeros(rows, cols-1, float64)
   var y = zeros(rows, 1, float64)
@@ -28,6 +29,7 @@ var (x_train, y_train) = read_csv("./data/iris_training.csv", 120, 5)
 
 var (x_test, y_test) = read_csv("./data/iris_test.csv", 30, 5)
 
+# Get the shape of a matrix
 proc shape(X: Matrix[float64]): (int, int) =
   var c = 0
   var r = 0
@@ -37,6 +39,7 @@ proc shape(X: Matrix[float64]): (int, int) =
     c+=1
   return (r, c)
 
+# Normalize matrix columns
 proc normalize(X: Matrix[float64]): Matrix[float64] =
   var (rows, cols) = shape(X)
   var nX = zeros(rows, cols)
@@ -45,6 +48,7 @@ proc normalize(X: Matrix[float64]): Matrix[float64] =
       nX[row, col] = X[row, col] / max(X.column(col))
   return nX
 
+# Adds a column of 1s to a matrix
 proc addOnes(X: Matrix[float64]): Matrix[float64] =
   var (rows, cols) = shape(X)
   var nX = zeros(rows, cols+1)
@@ -57,19 +61,7 @@ proc addOnes(X: Matrix[float64]): Matrix[float64] =
         nX[row, col] = X[row, col]
   return nX
 
-
-# echo x_train
-# var test = matrix(@[
-#         @[4.0, 6.0, 2.0, 5.0],
-#         @[3.0, 6.0, 4.0, 1.0],
-#         @[9.0, 3.0, 5.0, 2.0]])
-
-# test = normalize(test)
-
-# echo test
-
-# echo addOnes(normalize(test))
-
+# Batch gradient descent
 proc BGD(X: Matrix[float64], y: Matrix[float64], n_iter: int, eta: float64): Matrix[float64] =
   var nX = addOnes(normalize(X))
   var (rows, cols) = shape(nX)
@@ -80,10 +72,30 @@ proc BGD(X: Matrix[float64], y: Matrix[float64], n_iter: int, eta: float64): Mat
       theta[0,c] = theta[0,c] - eta * grad[0, 0]
   return theta.t
 
-var theta = BGD(x_train, y_train, 5, 0.01)
+# Get the theta for x_train
+var theta = BGD(x_train, y_train, 50, 0.01)
 
+# Make predictions for X using theta
 proc predict(X: Matrix[float64], theta: Matrix[float64]): Matrix[float64] =
   return addOnes(normalize(X)) * theta
 
-var preds = predict(x_test, theta)
+# Round a column
+proc roundCol(X: Matrix[float64], col: int): Matrix[float64] =
+  var (rows, cols) = shape(X)
+  var nX = zeros(rows, cols)
+  for row in 0..rows-1:
+    nX[row, col] = round(X[row, col])
+  return nX
 
+# Get predictions and round x_test
+var preds = roundCol(predict(x_test, theta), 0)
+
+# Accuracy score
+proc accuracy(p: Matrix[float64], t: Matrix[float64]): float64 =
+  var (rows, cols) = shape(p)
+  var c = 0
+  for row in 0..rows-1:
+    if p[row, 0] == t[row, 0]: c+=1
+  return c / rows
+
+echo "Acc: ", accuracy(preds, y_test)
